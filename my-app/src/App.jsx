32868915fd51856d3127f864c1ac81a9020898a5
Calvin Chang <tocalvin77@gmail.com>
2025-04-05 11:55:55 -0700

    edited stuff

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-r from-purple-500 to-pink-500 flex items-center justify-center text-white text-4xl font-bold">
-      Tailwind is working 🎉
+    <div>
+      <button className="square">X</button>
     </div>
   );
 }
+
 
 export default App
