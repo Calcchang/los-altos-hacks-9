c6a92c77254819985afaf62f677377d24fa773df
Calvin Chang <tocalvin77@gmail.com>
2025-04-05 10:59:14 -0700

    Switched to vite

---
+++ my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-r from-purple-500 to-pink-500 flex items-center justify-center text-white text-4xl font-bold">
+      Tailwind is working 🎉
+    </div>
+  );
+}
+
+export default App
